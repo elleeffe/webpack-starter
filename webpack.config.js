7be78bc44458364419fc4626d384be96cceb3dca
Faenzi Lorenzo <faenzilorenzo@gmail.com>
2022-02-09 14:47:36 +0100

    add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: {
+      import: './src/index.js',
+      // dependOn: 'shared',
+    },
+    // shared: 'nome pacchetto condiviso dagli entry point'
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Basic webpack config',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.css'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -22,5 +42,9 @@ module.exports = {
       },
       // To import CSVs, TSVs, and XML you could use the csv-loader and xml-loader.
     ],
+  },
+  devServer: {
+    static: './dist',
+    compress: true,
   },
 };
